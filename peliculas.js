4831feb41ead1c3a8efa2d4e79aa562cddc2c437
Jguerrero45 <guerrerojosegarcia@gmail.com>
2024-09-19 15:20:54 -0400

    Cambiado los colores de la grafica y se realizo de forma responsive

--- peliculas.js
+++ peliculas.js
@@ -47,13 +47,14 @@ class Llamada {
 
         listadata.forEach(movie => {
             const awards = movie.awards;
-            //sumar nominados
+            // sumar nominados
             let nominationsMatch = awards.includes('Nominated') ? awards.match(/Nominated for (\d+)/) : null;
             totalnominacion += nominationsMatch ? parseInt(nominationsMatch[1]) : 0;
-            //sumar ganadores
+            // sumar ganadores
             let winsMatch = awards.includes('Won') ? awards.match(/Won (\d+)/) : null;
             totalpremios += winsMatch ? parseInt(winsMatch[1]) : 0;
         });
+
         const gra = document.getElementById('grafico').getContext('2d');
         this.chartInstance ? (
             this.chartInstance.data.datasets[0].data = [totalnominacion, totalpremios],
@@ -67,11 +68,13 @@ class Llamada {
                         label: 'Premios y Nominaciones',
                         data: [totalnominacion, totalpremios],
                         borderWidth: 1,
-                        backgroundColor: ['rgba(126, 0, 183, 0.5)', 'rgba(169, 0, 169, 0.5)'],
-                        borderColor: ['rgba(74, 0, 187, 1)', 'rgba(187, 0, 187, 1)'],
+                        backgroundColor: ['rgba(88, 241, 183, 0.5)', 'rgba(2, 155, 207, 0.5)'],
+                        borderColor: ['rgba(2, 155, 207, 1)', 'rgba(88, 241, 183, 1)'],
                     }]
                 },
                 options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
                     scales: {
                         y: {
                             beginAtZero: true
@@ -81,125 +84,125 @@ class Llamada {
             })
         );
     }
-        agregarFila(data) {
-            const tabla = document.getElementById('tablaPeliculas').getElementsByTagName('tbody')[0];
-            const nuevaFila = tabla.insertRow();
+    agregarFila(data) {
+        const tabla = document.getElementById('tablaPeliculas').getElementsByTagName('tbody')[0];
+        const nuevaFila = tabla.insertRow();
 
-            const celdaId = nuevaFila.insertCell(0);
-            const celdaTitulo = nuevaFila.insertCell(1);
-            const celdaPremio = nuevaFila.insertCell(2);
+        const celdaId = nuevaFila.insertCell(0);
+        const celdaTitulo = nuevaFila.insertCell(1);
+        const celdaPremio = nuevaFila.insertCell(2);
 
-            celdaId.textContent = data.id;
-            celdaTitulo.textContent = data.title;
-            celdaPremio.textContent = data.awards;
+        celdaId.textContent = data.id;
+        celdaTitulo.textContent = data.title;
+        celdaPremio.textContent = data.awards;
 
-            const celdaIconos = nuevaFila.insertCell(3);
-            const ojoIcon = document.createElement('i');
-            ojoIcon.className = 'fa-solid fa-eye';
-            ojoIcon.style.cursor = 'pointer';
-            ojoIcon.style.marginRight = '10px';
-            ojoIcon.style.color = '#87CEEB';
-            const xIcon = document.createElement('i');
-            xIcon.className = 'fa-solid fa-trash';
-            xIcon.style.cursor = 'pointer';
-            xIcon.style.color = 'red';
+        const celdaIconos = nuevaFila.insertCell(3);
+        const ojoIcon = document.createElement('i');
+        ojoIcon.className = 'fa-solid fa-eye';
+        ojoIcon.style.cursor = 'pointer';
+        ojoIcon.style.marginRight = '10px';
+        ojoIcon.style.color = '#87CEEB';
+        const xIcon = document.createElement('i');
+        xIcon.className = 'fa-solid fa-trash';
+        xIcon.style.cursor = 'pointer';
+        xIcon.style.color = 'red';
 
-            celdaIconos.appendChild(ojoIcon);
-            celdaIconos.appendChild(xIcon);
+        celdaIconos.appendChild(ojoIcon);
+        celdaIconos.appendChild(xIcon);
 
-            ojoIcon.addEventListener('click', () => {
-                this.mostrarInformacion(data);
-            });
+        ojoIcon.addEventListener('click', () => {
+            this.mostrarInformacion(data);
+        });
 
-            xIcon.addEventListener('click', () => {
-                console.log('Ícono de X roja clickeado');
-                const idFila = parseInt(celdaId.textContent, 10);
-                this.usedIds.delete(idFila);
+        xIcon.addEventListener('click', () => {
+            console.log('Ícono de X roja clickeado');
+            const idFila = parseInt(celdaId.textContent, 10);
+            this.usedIds.delete(idFila);
 
-                // Eliminar la película de listadata
-                const index = this.listadata.findIndex(movie => movie.id === idFila);
-                (index !== -1) && this.listadata.splice(index, 1);
+            // Eliminar la película de listadata
+            const index = this.listadata.findIndex(movie => movie.id === idFila);
+            (index !== -1) && this.listadata.splice(index, 1);
 
-                // Actualizar la gráfica
-                this.filtropremios(this.listadata);
+            // Actualizar la gráfica
+            this.filtropremios(this.listadata);
 
-                tabla.removeChild(nuevaFila);
-                this.usedIds.size < this.maxIds ? retomarLlamadas() : null;
-            });
+            tabla.removeChild(nuevaFila);
+            this.usedIds.size < this.maxIds ? retomarLlamadas() : null;
+        });
 
-            this.ordenarTabla();
+        this.ordenarTabla();
 
 
-            // Ordenar la tabla después de añadir la fila
-            this.ordenarTabla();
+        // Ordenar la tabla después de añadir la fila
+        this.ordenarTabla();
 
-        }
-
-        mostrarInformacion(data) {
-            console.log(data);
-            const infoVentana = document.getElementById('infoVentana');
-            document.getElementById('infoTitulo').textContent = data.title;
-            document.getElementById('infoAnio').textContent = data.year;
-            document.getElementById('infoGenero').textContent = data.genre.join(', ');
-            document.getElementById('infoRating').textContent = data.rating;
-            document.getElementById('infoDirector').textContent = data.director;
-            document.getElementById('infoActores').textContent = data.actors.join(', ');
-            document.getElementById('infoSinopsis').textContent = data.plot;
-            document.getElementById('infoDuracion').textContent = data.runtime + ' minutos';
-            document.getElementById('infoPremios').textContent = data.awards;
-            document.getElementById('infoPais').textContent = data.country;
-            document.getElementById('infoIdioma').textContent = data.language;
-            document.getElementById('infoTaquilla').textContent = data.boxOffice;
-            document.getElementById('infoProduccion').textContent = data.production;
-            document.getElementById('infoWebsite').href = data.website;
-            document.getElementById('infoWebsite').textContent = data.website;
-            document.getElementById('infoPoster').src = data.poster;
-            document.getElementById('infoPoster').style.display = 'flex';
-            document.getElementById('infoPoster').style.justifyContent = 'center';
-            document.getElementById('infoPoster').style.alignItems = 'center';
-
-            document.getElementById('infoTrailer').href = data.trailer;
-
-            infoVentana.style.display = 'block'; // Mostrar la ventana emergente
-        }
-        cerrarInformacion() {
-            const infoVentana = document.getElementById('infoVentana');
-            infoVentana.style.display = 'none'; // Ocultar la ventana emergente
-        }
-
-        buscarPelicula(titulo) {
-            const tabla = document.getElementById('tablaPeliculas').getElementsByTagName('tbody')[0];
-            const filas = Array.from(tabla.rows);
-
-            // Eliminar el resaltado de la fila anterior si existe
-            filas.forEach(fila => fila.classList.remove('fila-seleccionada'));
-
-            const filaEncontrada = filas.find(fila => fila.cells[1].textContent.toLowerCase() === titulo.toLowerCase());
-
-            filaEncontrada
-                ? (filaEncontrada.classList.add('fila-seleccionada'), filaEncontrada.scrollIntoView({ behavior: 'smooth', block: 'center' }))
-                : alert('No se encontró ninguna película con ese título.');
-        }
-
-        ordenarTabla() {
-            const tabla = document.getElementById('tablaPeliculas').getElementsByTagName('tbody')[0];
-            // Convertir las filas en un array
-            const filas = Array.from(tabla.rows);
-
-            // Ordenar filas basadas en el texto de la segunda celda
-            const filasOrdenadas = filas.sort((a, b) => {
-                const nombreA = a.cells[1].textContent.toLowerCase();
-                const nombreB = b.cells[1].textContent.toLowerCase();
-                return nombreA.localeCompare(nombreB);
-            });
-
-            // Actualizar la tabla con las filas ordenadas
-            tabla.innerHTML = '';
-            filasOrdenadas.forEach(fila => tabla.appendChild(fila));
-        }
     }
 
-    const miLlamada = new Llamada();
+    mostrarInformacion(data) {
+        console.log(data);
+        const infoVentana = document.getElementById('infoVentana');
+        document.getElementById('infoTitulo').textContent = data.title;
+        document.getElementById('infoAnio').textContent = data.year;
+        document.getElementById('infoGenero').textContent = data.genre.join(', ');
+        document.getElementById('infoRating').textContent = data.rating;
+        document.getElementById('infoDirector').textContent = data.director;
+        document.getElementById('infoActores').textContent = data.actors.join(', ');
+        document.getElementById('infoSinopsis').textContent = data.plot;
+        document.getElementById('infoDuracion').textContent = data.runtime + ' minutos';
+        document.getElementById('infoPremios').textContent = data.awards;
+        document.getElementById('infoPais').textContent = data.country;
+        document.getElementById('infoIdioma').textContent = data.language;
+        document.getElementById('infoTaquilla').textContent = data.boxOffice;
+        document.getElementById('infoProduccion').textContent = data.production;
+        document.getElementById('infoWebsite').href = data.website;
+        document.getElementById('infoWebsite').textContent = data.website;
+        document.getElementById('infoPoster').src = data.poster;
+        document.getElementById('infoPoster').style.display = 'flex';
+        document.getElementById('infoPoster').style.justifyContent = 'center';
+        document.getElementById('infoPoster').style.alignItems = 'center';
+
+        document.getElementById('infoTrailer').href = data.trailer;
+
+        infoVentana.style.display = 'block'; // Mostrar la ventana emergente
+    }
+    cerrarInformacion() {
+        const infoVentana = document.getElementById('infoVentana');
+        infoVentana.style.display = 'none'; // Ocultar la ventana emergente
+    }
+
+    buscarPelicula(titulo) {
+        const tabla = document.getElementById('tablaPeliculas').getElementsByTagName('tbody')[0];
+        const filas = Array.from(tabla.rows);
+
+        // Eliminar el resaltado de la fila anterior si existe
+        filas.forEach(fila => fila.classList.remove('fila-seleccionada'));
+
+        const filaEncontrada = filas.find(fila => fila.cells[1].textContent.toLowerCase() === titulo.toLowerCase());
+
+        filaEncontrada
+            ? (filaEncontrada.classList.add('fila-seleccionada'), filaEncontrada.scrollIntoView({ behavior: 'smooth', block: 'center' }))
+            : alert('No se encontró ninguna película con ese título.');
+    }
+
+    ordenarTabla() {
+        const tabla = document.getElementById('tablaPeliculas').getElementsByTagName('tbody')[0];
+        // Convertir las filas en un array
+        const filas = Array.from(tabla.rows);
+
+        // Ordenar filas basadas en el texto de la segunda celda
+        const filasOrdenadas = filas.sort((a, b) => {
+            const nombreA = a.cells[1].textContent.toLowerCase();
+            const nombreB = b.cells[1].textContent.toLowerCase();
+            return nombreA.localeCompare(nombreB);
+        });
+
+        // Actualizar la tabla con las filas ordenadas
+        tabla.innerHTML = '';
+        filasOrdenadas.forEach(fila => tabla.appendChild(fila));
+    }
+}
+
+const miLlamada = new Llamada();
 
 
 // Manejador para cerrar la ventana emergente
@@ -217,7 +220,7 @@ function llamarCada5Segundos() {
 }
 
 function retomarLlamadas() {
-    
+
     miLlamada.intervalId = setInterval(llamarCada5Segundos, 5000);
 }
 retomarLlamadas();
